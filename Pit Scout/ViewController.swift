89a6ad40754ec2f225331a3394b20fdffdf52017
Casey Cooper <caseycooper@caseys-air-2.staff.fusd.local>
2018-01-17 16:35:44 -0800

    Initial Commit

---
+++ Pit Scout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pit Scout
+//
+//  Created by Casey Cooper on 1/17/18.
+//  Copyright © 2018 Casey Cooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
